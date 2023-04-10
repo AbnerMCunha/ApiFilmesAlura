acf4b745fceed2e5b765cba19da40638061f3701
Abner Cunha <abnermcunha@gmail.com>
2023-04-09 22:32:22 -0300

    Criando Model e Controller de Filmes

---
+++ FilmesApi/FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FilmesApi.Models;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController {
+
+    private List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody]Filme filme)    //Anotação FromBody representa que a info será lançada a partir do corpo da requisição
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme);
+        Console.WriteLine("Titulo: " + filme.Titulo);
+        Console.WriteLine("Genero: " + filme.Genero);
+        Console.WriteLine("Duracao: " + filme.Duracao.ToString());
+    }
+
+    [HttpGet]
+    public List<Filme> RecuperaFilmes()
+    {
+        return filmes;
+    }
+
+
+}
---
+++ FilmesApi/FilmesApi/Models/Filme.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models {
+    public class Filme {
+
+        [Required(ErrorMessage ="O Título do Filme é Obrigatório")]
+        
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O Genero do Filme é Obrigatório")]
+        [MaxLength(50, ErrorMessage = "Tamanho Maximo é de 50")]
+        public string Genero { get; set; }
+        
+        [Required(ErrorMessage = "A duração do Filme é Obrigatório")]
+        [Range(70, 600, ErrorMessage = "Duração de Filme devem estar dentre 70 a 600 Minutos!")]
+        public int Duracao { get; set; }
+
+    }
+}
